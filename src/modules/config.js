b47571050f0c968344328c6d6984fae7fc97cc81
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-20 11:16:10 +0200

    Use https in async calls
    Fixing this problem
    
    `This request has been blocked; the content must be served over HTTPS.`

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,6 +1,6 @@
 const appConfig = () => {
   const getAPIKey = () => '65ae24246c1f9d44901df470b6f262de';
-  const getWeatherURL = () => 'http://api.openweathermap.org/data/2.5/weather?';
+  const getWeatherURL = () => 'https://api.openweathermap.org/data/2.5/weather?';
   const getGiphyURL = () => 'https://api.giphy.com/v1/gifs/search?';
   const getGiphyAPIKey = () => 'dN2DhkuIl9Cqh9h2bLBxh1T2takgbHw6';
   return {
--- src/modules/search.js
+++ src/modules/search.js
@@ -15,6 +15,17 @@ const searchForm = () => {
         display.toggleTemperature();
       };
     };
+
+    const loadPictureFromGiphy = async (cityConditions) => {
+      const pictureAPIKey = config.getGiphyAPIKey();
+      const pictureAPISite = config.getGiphyURL();
+      const pictureRequestURL = `${pictureAPISite}q=${cityConditions}&api_key=${pictureAPIKey}&limit=1`;
+      const picture = await fetch(pictureRequestURL, { mode: 'cors' });
+      const pictureResults = await picture.json();
+      const cityPicture = pictureResults.data[0].images.original.url;
+      return cityPicture;
+    };
+
     const startSearch = async () => {
       try {
         const response = await fetch(weatherRequestURL, { mode: 'cors' });
@@ -22,14 +33,10 @@ const searchForm = () => {
         const results = await response.json();
 
         const cityName = `${results.name}, ${results.sys.country}`;
-        const pictureAPIKey = config.getGiphyAPIKey();
-        const pictureAPISite = config.getGiphyURL();
+
         const cityConditions = results.weather[0].main;
-        const pictureRequestURL = `${pictureAPISite}q=${cityConditions}&api_key=${pictureAPIKey}&limit=1`;
-        const picture = await fetch(pictureRequestURL, { mode: 'cors' });
-        const pictureResults = await picture.json();
+        const cityPicture = await loadPictureFromGiphy(cityConditions);
         const cityTemp = results.main.temp;
-        const cityPicture = pictureResults.data[0].images.original.url;
         const tempSymbol = '°C';
         display.insertSearchResults(cityName, cityConditions, cityTemp, tempSymbol, cityPicture);
         initTemperatureListener(cityTemp);
